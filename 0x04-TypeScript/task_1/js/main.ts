e6dfb12590d3a6161d209ffdad65f127503f467b
Okugbe <egedegbeoghenero@gmail.com>
2024-10-10 05:55:07 +0100

    Typescript task omit

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -15,4 +15,42 @@ const teacher3: Teacher = {
   contract: false,
 };
 
-console.log(teacher3);
+
+interface Directors extends Teacher {
+  numberOfReports: number
+}
+
+function printTeacher(firstName: string, lastName: string): string {
+  return `${firstName[0]}. ${lastName}`
+}
+
+interface printTeacherFunction {
+  (firstName: string, lastName:string): string
+}
+
+class StudentClass implements StudentInterface{
+  firstName: string
+  lastName: string
+
+  constructor(firstName: string, lastName: string) {
+      this.firstName = firstName;
+      this.lastName = lastName;
+  }
+
+  workOnHomework(): string {
+      return 'Currently working';
+  }
+
+  displayName(): string {
+      return this.firstName;
+  }
+}
+
+interface StudentConstructor {
+  new (firstName:string, lastName: string): StudentClass
+}
+
+interface StudentInterface {
+  workOnHomework(): string,
+  displayName(): string
+}
--- 0x04-TypeScript/task_2/js/main.ts
+++ 0x04-TypeScript/task_2/js/main.ts
@@ -1,22 +1,62 @@
-interface Teacher {
-  readonly firstName: string;
-  readonly lastName: string;
-  fullTimeEmployee: boolean;
-  yearsOfExperience?: number;
-  location: string;
-  [key: string]: any;
+interface DirectorInterface {
+  workFromHome(): string,
+  getCoffeeBreak(): string,
+  workDirectorTasks(): string
 }
 
-interface Directors extends Teacher {
-  numberOfReports: number;
+interface TeacherInterface {
+  workFromHome(): string,
+  getCoffeeBreak(): string,
+  workTeacherTasks(): string
 }
 
-const director1: Directors = {
-  firstName: 'John',
-  lastName: 'Doe',
-  location: 'London',
-  fullTimeEmployee: true,
-  numberOfReports: 17,
-};
+class Director implements DirectorInterface {
+  workFromHome(): string {
+      return 'Working from home';
+  }
+  getCoffeeBreak(): string {
+      return 'Getting a coffee break';
+  }
+  workDirectorTasks(): string {
+      return 'Getting to director tasks';
+  }
+}
 
-console.log(director1);
+class Teacher implements TeacherInterface {
+  workFromHome(): string {
+      return 'Cannot work from home';
+  }
+  getCoffeeBreak(): string {
+      return 'Cannot have a break';
+  }
+  workTeacherTasks(): string {
+      return 'Getting to work';
+  }
+}
+
+function createEmployee(salary: (number | string)): (Director | Teacher) {
+  if (typeof salary === 'number' && salary < 500) {
+      return new Teacher();
+  }
+  return new Director();
+}
+
+function isDirector(employee: (Director | Teacher)): boolean {
+  return employee instanceof Director;
+}
+
+function executeWork(employee: (Director | Teacher)): string {
+  if (isDirector(employee)) {
+      return (employee as Director).workDirectorTasks();
+  }
+  return (employee as Teacher).workTeacherTasks();
+}
+
+type Subjects = ('Math' | 'History');
+
+function teachClass(todayClass: Subjects) {
+  if (todayClass === 'Math') {
+      return 'Teaching Math';
+  }
+  return 'Teaching History';
+}
